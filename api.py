f71b1ed4c1a79affb08dde8337157671069027ce
James Heggs <james.heggs@techreturners.com>
2021-04-04 17:20:38 +0100

    Introducing prometheus exporter to python app

--- api.py
+++ api.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, marshal, fields
 from flask_cors import CORS
+from prometheus_flask_exporter import RESTfulPrometheusMetrics
 
 # Initialize Flask
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
+metrics = RESTfulPrometheusMetrics(app, api)
+
+metrics.info('app_info', 'Application info', version='1.4')
 
 # A List of Dicts to store all of the books
 books = [{
@@ -41,8 +45,11 @@ class BookList(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
 
+    @metrics.summary('requests_by_status', 'Request latencies by status', labels={'status': lambda r: r.status_code})
     def get(self):
-        return{"books": [marshal(book, bookFields) for book in books]}
+        return {
+            "books": [marshal(book, bookFields) for book in books]
+        }, 200
 
 
 api.add_resource(BookList, "/books")
